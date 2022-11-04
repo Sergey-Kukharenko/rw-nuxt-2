127149d41b4e01ee206268d791ed0726dfa26acc
Sergey Kukharenko <iamksergey@mail.ru>
2022-11-04 16:10:17 +0300

    init

--- pages/_id.vue
+++ pages/_id.vue
@@ -38,6 +38,7 @@
 </template>
 
 <script>
+import bouquetSunshine from '@/data/bouquet-sunshine';
 import AppFormOffers from '~/components/product/AppFormOffers';
 import AppService from '~/components/product/AppService';
 
@@ -48,19 +49,30 @@ export default {
 
   middleware: ['not-found'],
 
-   async asyncData({route, $axios, redirect}) {
-     const path = route.fullPath;
+    asyncData({route, $axios, redirect}) {
+     // const path = route.fullPath;
+     //
+     // return await $axios.$get(`${process.env.CARD_PRODUCT_DEV_URL}${path}`)
+     //   .then(({data}) => {
+     //     return {
+     //       seo: data.seo,
+     //       title: data.title,
+     //       description: data.description,
+     //       object: data.object,
+     //       positions: data.positions,
+     //     };
+     //   })
 
-     return await $axios.$get(`${process.env.CARD_PRODUCT_DEV_URL}${path}`)
-       .then(({data}) => {
-         return {
-           seo: data.seo,
-           title: data.title,
-           description: data.description,
-           object: data.object,
-           positions: data.positions,
-         };
-       })
+
+     const {data} = bouquetSunshine;
+
+     return {
+       seo: data.seo,
+       title: data.title,
+       description: data.description,
+       object: data.object,
+       positions: data.positions,
+     };
    },
 
   head() {
