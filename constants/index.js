e7f50bf35443497f903d1ed1a3dcc414f0f6ec6f
Sergey Kukharenko <iamksergey@mail.ru>
2022-11-05 12:16:37 +0300

    init

--- constants/index.js
+++ constants/index.js
@@ -1,16 +1,16 @@
-const LIST_ITEM_VIEW_COUNT = 5
+const LIST_ITEM_VIEW_COUNT = 5;
 
 const FILTER_TYPES = {
-    checkbox: 'checkbox',
-    range: 'range',
-    radio: 'radio'
-}
+  checkbox: 'checkbox',
+  range: 'range',
+  radio: 'radio'
+};
 
-const CARD_PRODUCT_DEV_URL = "https://dev-api.myflowers.co.uk/v1/offers"
+const CARD_PRODUCT_DEV_URL = 'https://dev-api.myflowers.co.uk/v1/offers';
 
 export {
-    LIST_ITEM_VIEW_COUNT,
-    FILTER_TYPES,
+  LIST_ITEM_VIEW_COUNT,
+  FILTER_TYPES,
   CARD_PRODUCT_DEV_URL
-}
+};
 
