4b45f9376be05c92ca8e18b97e6599e6fba5198a
Sergey Kukharenko <iamksergey@mail.ru>
2022-11-28 23:15:04 +0300

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,16 +71,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    axios: {
-      baseUrl: '/dev-api',
-      proxy: true
-    },
-    proxy: {
-      "/dev-api/": {
-        target: "https://dev-api.myflowers.co.uk/v1",
-        pathRewrite: { "^/dev-api/": "" }
-      }
-    },
+    baseURL: process.env.BASE_URL || '/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
