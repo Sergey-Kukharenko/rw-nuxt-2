8f71573fc8006663c22b8c443e45334be856a635
Sergey Kukharenko <iamksergey@mail.ru>
2022-11-29 11:45:29 +0300

    init

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,7 +1,8 @@
 export default function ({$axios, redirect}) {
   $axios.onError(error => {
     console.log('middleware');
-    console.log(error.response.status);
+    console.log(error);
+    // console.log(error.response.status);
     if (error.response.status >= 400) {
       redirect('/not-found');
     }
