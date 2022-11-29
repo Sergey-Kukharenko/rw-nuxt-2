999429130da3546875198ac34b3243df8f0cc8bb
Sergey Kukharenko <iamksergey@mail.ru>
2022-11-29 11:00:14 +0300

    init

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,7 +1,6 @@
 export default function ({$axios, redirect}) {
   $axios.onError(error => {
     if (error.response.status >= 400) {
-      console.log(error.response.status === 502);
       redirect('/not-found');
     }
   });
--- nuxt.config.js
+++ nuxt.config.js
@@ -61,6 +61,7 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/svg-sprite',
     '@nuxtjs/device',
+    '@nuxtjs/proxy'
   ],
 
   svgSprite: {
@@ -71,7 +72,15 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.BASE_URL || '/'
+    baseURL: process.env.BASE_URL || '/',
+    proxy: true
+  },
+
+  proxy: {
+    '/dev-api/': {
+      target: 'https://dev-api.myflowers.co.uk/v1/',
+      pathRewrite: { '^/dev-api/': '' }
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
