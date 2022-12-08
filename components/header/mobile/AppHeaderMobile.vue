b46f670aedf1c0d191e796c31df02ff500de3f91
Sergey Kukharenko <iamksergey@mail.ru>
2022-12-08 16:58:17 +0300

    init

--- components/header/mobile/AppHeaderMobile.vue
+++ components/header/mobile/AppHeaderMobile.vue
@@ -25,15 +25,16 @@
       </div>
     </app-drawer>
 
-    <app-header-checkout-order v-show="isCheckout"/>
-    <div v-show="!isCheckout" class="header-row">
-      <app-logo/>
-      <nuxt-link to="/search" class="search">
-        <svg-icon name="search" class="search__icon"/>
-      </nuxt-link>
-      <app-call/>
-      <app-cart/>
-    </div>
+    {{$route.name}}
+<!--    <app-header-checkout-order v-show="isCheckout"/>-->
+<!--    <div v-show="!isCheckout" class="header-row">-->
+<!--      <app-logo/>-->
+<!--      <nuxt-link to="/search" class="search">-->
+<!--        <svg-icon name="search" class="search__icon"/>-->
+<!--      </nuxt-link>-->
+<!--      <app-call/>-->
+<!--      <app-cart/>-->
+<!--    </div>-->
   </header>
 </template>
 
@@ -41,27 +42,27 @@
 import {useToggleClassName} from '~/helpers';
 
 import AppDrawer from '~/components/shared/AppDrawer';
-import AppLogo from '~/components/header/AppLogo';
-import AppCall from '~/components/header/AppCall';
-import AppCart from '~/components/header/AppCart';
+// import AppLogo from '~/components/header/AppLogo';
+// import AppCall from '~/components/header/AppCall';
+// import AppCart from '~/components/header/AppCart';
 import AppMobileProfile from '~/components/header/mobile/AppMobileProfile';
 import AppMobileLocation from '~/components/header/mobile/AppMobileLocation';
 import AppHeaderMobileMenu from '~/components/header/mobile/AppHeaderMobileMenu';
 import AppHeaderMobileNav from '~/components/header/mobile/AppHeaderMobileNav';
-import AppHeaderCheckoutOrder from '~/components/header-checkout/AppHeaderCheckoutOrder';
+// import AppHeaderCheckoutOrder from '~/components/header-checkout/AppHeaderCheckoutOrder';
 
 export default {
   name: 'AppHeaderMobile',
 
   components: {
-    AppHeaderCheckoutOrder,
+    // AppHeaderCheckoutOrder,
     AppHeaderMobileNav,
     AppHeaderMobileMenu,
     AppMobileLocation,
     AppMobileProfile,
-    AppCart,
-    AppCall,
-    AppLogo,
+    // AppCart,
+    // AppCall,
+    // AppLogo,
     AppDrawer
   },
 
