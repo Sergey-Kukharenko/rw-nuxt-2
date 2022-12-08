30cf720b10040d919b988b20eee3a44781938e8d
Sergey Kukharenko <iamksergey@mail.ru>
2022-12-08 19:44:54 +0300

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,39 @@
 <template>
   <div>
-<!--    <template v-if="$device.isMobileOrTablet">-->
-<!--      <app-header-mobile/>-->
-<!--      <app-notification/>-->
-<!--    </template>-->
+    <template v-if="$device.isMobileOrTablet">
+      <app-header-mobile/>
+      <app-notification/>
+    </template>
 
-<!--    <template v-else>-->
-<!--      <app-notification/>-->
-<!--      <app-header class="header-group"/>-->
-<!--      <app-nav-bar/>-->
-<!--    </template>-->
+    <template v-else>
+      <app-notification/>
+      <app-header class="header-group"/>
+      <app-nav-bar/>
+    </template>
 
-<!--    <app-breadcrumbs/>-->
+    <app-breadcrumbs/>
     <Nuxt/>
-<!--    <app-footer/>-->
+    <app-footer/>
   </div>
 </template>
 
 <script>
-// import AppHeader from '@/components/header/AppHeader';
-// import AppHeaderMobile from '~/components/header/mobile/AppHeaderMobile';
-// import AppNavBar from '@/components/header/AppNavBar';
-// import AppBreadcrumbs from '~/components/shared/AppBreadcrumbs';
-// import AppFooter from '@/components/footer/AppFooter';
-// import AppNotification from '~/components/header/AppNotification';
+import AppHeader from '@/components/header/AppHeader';
+import AppHeaderMobile from '~/components/header/mobile/AppHeaderMobile';
+import AppNavBar from '@/components/header/AppNavBar';
+import AppBreadcrumbs from '~/components/shared/AppBreadcrumbs';
+import AppFooter from '@/components/footer/AppFooter';
+import AppNotification from '~/components/header/AppNotification';
 
 export default {
   name: 'DefaultLayout',
   components: {
-    // AppNotification,
-    // AppNavBar,
-    // AppHeaderMobile,
-    // AppHeader,
-    // AppBreadcrumbs,
-    // AppFooter,
+    AppNotification,
+    AppNavBar,
+    AppHeaderMobile,
+    AppHeader,
+    AppBreadcrumbs,
+    AppFooter,
   }
 };
 </script>
