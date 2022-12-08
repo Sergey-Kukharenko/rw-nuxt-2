df9cb0631b299eb98bea9f9e4e212fd8c21dba2a
Sergey Kukharenko <iamksergey@mail.ru>
2022-12-08 16:19:24 +0300

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,22 @@
 <template>
   <div>
     <template v-if="$device.isMobileOrTablet">
-      <app-header-mobile/>
-      <app-notification/>
+      Mobile
     </template>
 
     <template v-else>
-      <app-notification/>
-      <app-header class="header-group"/>
-      <app-nav-bar/>
+      DESKTOP
     </template>
-
-<!--    <app-breadcrumbs/>-->
-<!--    <Nuxt/>-->
-<!--    <app-footer/>-->
   </div>
 </template>
 
 <script>
-import AppHeader from '@/components/header/AppHeader';
-import AppHeaderMobile from '~/components/header/mobile/AppHeaderMobile';
-import AppNavBar from '@/components/header/AppNavBar';
-// import AppBreadcrumbs from '~/components/shared/AppBreadcrumbs';
-// import AppFooter from '@/components/footer/AppFooter';
-import AppNotification from '~/components/header/AppNotification';
+
 
 export default {
   name: 'DefaultLayout',
   components: {
-    AppNotification,
-    AppNavBar,
-    AppHeaderMobile,
-    AppHeader,
-    // AppBreadcrumbs,
-    // AppFooter,
+
   }
 };
 </script>
