78641f68368351e111758744a910de3415ad9c05
Sergey Kukharenko <iamksergey@mail.ru>
2022-12-09 10:54:15 +0300

    init

--- components/header/mobile/AppHeaderMobile.vue
+++ components/header/mobile/AppHeaderMobile.vue
@@ -35,12 +35,6 @@
       <app-cart/>
     </template>
 
-    <app-logo/>
-    <nuxt-link to="/search" class="search">
-      <svg-icon name="search" class="search__icon"/>
-    </nuxt-link>
-    <app-call/>
-    <app-cart/>
   </header>
 </template>
 
