1f1324a0020af153011fb8227c26b6d79e8996b6
Sergey Kukharenko <iamksergey@mail.ru>
2022-12-09 11:16:40 +0300

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <template v-if="$device.isMobileOrTablet">
       <app-header-mobile/>
-      <app-notification/>
+<!--      <app-notification/>-->
     </template>
 
     <template v-else>
